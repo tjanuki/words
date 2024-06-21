c124a935f273a037580e25e0a11e45160878b4e1
tjanuki <t.januki@gmail.com>
2024-06-21 15:58:49 -0400

    Add requirements.txt

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,10 +1,10 @@
-import json
 import nltk
-from nltk.corpus import wordnet as wn
-from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
+from nltk.corpus import wordnet as wn
+from nltk.stem import WordNetLemmatizer
 
+# Function to convert nltk POS tags to WordNet POS tags
 def get_wordnet_pos(treebank_tag):
     if treebank_tag.startswith('J'):
         return wn.ADJ
@@ -17,31 +17,26 @@ def get_wordnet_pos(treebank_tag):
     else:
         return None
 
-def lambda_handler(event, context):
-    try:
-        sentence = event['sentence']
-        lemmatizer = WordNetLemmatizer()
+# Function to normalize a sentence
+def normalize_sentence(sentence):
+    lemmatizer = WordNetLemmatizer()
+    tokens = word_tokenize(sentence.lower())
+    tagged_tokens = pos_tag(tokens)
 
-        tokens = word_tokenize(sentence)
-        tagged_tokens = pos_tag(tokens)
+    normalized_tokens = []
+    for token, tag in tagged_tokens:
+        wn_tag = get_wordnet_pos(tag)
+        if wn_tag is None:
+            lemma = token
+        else:
+            lemma = lemmatizer.lemmatize(token, wn_tag)
+        normalized_tokens.append(lemma)
 
-        normalized_tokens = []
-        for token, tag in tagged_tokens:
-            wn_tag = get_wordnet_pos(tag)
-            if wn_tag is None:
-                lemma = token
-            else:
-                lemma = lemmatizer.lemmatize(token, wn_tag)
-            normalized_tokens.append(lemma)
+    return ' '.join(normalized_tokens)
 
-        normalized_sentence = ' '.join(normalized_tokens)
+# Test with a sample sentence
+sentence = "Told apples"
+normalized_sentence = normalize_sentence(sentence)
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps({'normalized': normalized_sentence})
-        }
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
-        }
+print(f"Original sentence: {sentence}")
+print(f"Normalized sentence: {normalized_sentence}")
